0d194f9ae3434de3ca470fd36b2cc3160d4bdb3d
GiovaneBonache <160802024+GiovaneBonache@users.noreply.github.com>
2026-04-07 22:39:51 -0300

    conexão entre vue e SpringBoot

--- src/main/java/com/trabalho/seguranca/security/FilterChain.java
+++ src/main/java/com/trabalho/seguranca/security/FilterChain.java
@@ -111,8 +111,10 @@ public class FilterChain {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "http://127.0.0.1:5173"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
